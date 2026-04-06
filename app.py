3868f62574f320fa96d93805928440795c154182
Ridwan Kazi <ridwank58@kprschools.ca>
2026-04-06 22:33:36 +0000

    more changes

--- app.py
+++ app.py
@@ -17,7 +17,27 @@ def menu():
     database.create_tables(connection)
 
     while (user_input := input(MENU_PROMPT)) != "5":
-        print(user_input)
+        if user_input == "1":
+            name = input("Enter bean name: ")
+            method = input("Enter how you've prepared it: ")
+            rating = int(input("Enter your rating score (0-100): "))
+            database.add_bean(connection, name, method, rating)
+        elif user_input == "2":
+            beans = database.get_all_beans(connection)
+            for bean in beans:
+                print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+        elif user_input == "3":
+            name = input("Enter bean name to find: ")
+            beans = database.get_beans_by_name(connection, name)
+            for bean in beans:
+                print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+        elif user_input == "4":
+            name = input("Enter bean name to find: ")
+            bean = database.get_best_preparation_for_bean(connection, name)
+            print(f"The best preparation method for {name} is: {best_method[2]}")
+  
+        else:
+            print("Invalid input, please try again.")
 
 menu()
        
